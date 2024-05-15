e540ad0e7ac76cfe4cb90f48995b9b172caa60b2
valentynbez <walentyn.bzsz@gmail.com>
2024-05-15 17:22:00 +0200

    feat: embedding module

--- tm_vec/embedding.py
+++ tm_vec/embedding.py
@@ -1,5 +1,4 @@
 from transformers import EsmTokenizer, EsmModel
-import gc
 import torch
 from abc import abstractmethod
 from tqdm import tqdm
@@ -9,7 +8,13 @@ import logging
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-class Model:
+class BERTModel:
+    """
+    Base class for protein LLMs based on BERT architecture.
+    Includes methods for tokenization and batching of sequences.
+    Tested on ESM and ProtT5
+    """
+
 
     def __init__(self, model_path, cache_dir, device):
         self.model_path = model_path
@@ -18,17 +23,36 @@ class Model:
         self.model = None
         self.device = device
 
-    def batch_input(self, sequences, batch_size):
-        for i in range(0, len(sequences), batch_size):
-            yield sequences[i:i + batch_size]
-
     def tokenize(self, sequences):
         inp = self.tokenizer(sequences, padding=True, return_tensors="pt")
         return inp
 
+    def forward_pass(self, inp):
+        with torch.no_grad():
+            out = self.model(**inp)
+        return out
+
+    def batch_embed(self, sequences):
+
+        inp = self.tokenize(sequences)
+        out = self.forward_pass(inp)
+        embs = out.last_hidden_state
+
+        return inp, embs
+
     @abstractmethod
-    def batch_embed(self, sequences, batch_size):
-        raise NotImplementedError
+    def remove_special_tokens(self, embedding, attention_mask):
+        # add a verbose error
+        raise NotImplementedError("Method for removing special tokens is not implemented, the embeddings should not be used as is.")
+
+    def get_sequence_embeddings(self, sequences):
+        """
+        Get embeddings from the last layer for sequences.
+        """
+        inp, embs = self.batch_embed(sequences)
+        # remove special tokens
+        embs = self.remove_special_tokens(embs, inp["attention_mask"])
+        return embs
 
 
 
@@ -45,34 +69,65 @@ class ESMEncoder(Model):
         self.model = EsmModel.from_pretrained(self.model_path, cache_dir=self.cache_dir)
         self.tokenizer = EsmTokenizer.from_pretrained(self.model_path, cache_dir=self.cache_dir, local_files_only=local_files_only,
                                                       add_pooling_layer=pooling_layer)
-        # convert to half precision if cuda
-        if self.device.type == "cuda":
-            self.model.half()
+        # # convert to half precision if cuda
+        # if self.device.type == "cuda":
+        #     self.model.half()
 
         self.model.to(self.device)
+        self.model.eval()
         # compile model
         self.model = torch.compile(self.model, mode="max-autotune", dynamic=True)
+
+
+        def remove_special_tokens(self, embedding, attention_mask):
+            """
+            Remove special tokens from the embedding
+            """
+            embeddings = []
+
+            for seq_num in range(len(embs)):
+                seq_len = (attention_mask[seq_num] == 1).sum()
+                seq_emb = embs[seq_num][: seq_len - 1]
+                # remove first <cls> token
+                embeddings.append(seq_emb[1:])
+
+            return embeddings
+
+
+class ProtT5Encoder:
+    def __init__(self,
+                 model_path: str,
+                 cache_dir: str,
+                 device : str,
+                 local_files_only: bool = True):
+
+        super().__init__(model_path, cache_dir, device)
+        self.model = ProtT5ForConditionalGeneration.from_pretrained(self.model_path, cache_dir=self.cache_dir)
+        self.tokenizer = ProtT5Tokenizer.from_pretrained(self.model_path, cache_dir=self.cache_dir, local_files_only=local_files_only)
+        # # convert to half precision if cuda
+        # if self.device.type == "cuda":
+        #     self.model.half()
+
+        self.model.to(self.device)
         self.model.eval()
-        gc.collect()
+        # compile model
+        self.model = torch.compile(self.model, mode="max-autotune", dynamic=True)
 
-    def batch_embed(self, sequences):
 
-        embeddings = []
-        inp = self.tokenize(sequences)
-        inp = {k: v.to(self.device) for k, v in inp.items()}
+        def remove_special_tokens(self, embedding, attention_mask):
+            """
+            Remove special tokens from the embedding
+            """
+            embeddings = []
 
-        with torch.no_grad():
-            out = self.model(**inp)
+            for seq_num in range(len(embs)):
+                seq_len = (attention_mask[seq_num] == 1).sum()
+                seq_emb = embs[seq_num][: seq_len - 1]
 
-        embs = out.last_hidden_state
+            return embeddings
 
-        for seq_num in range(len(embs)):
-            seq_len = (inp["attention_mask"][seq_num] == 1).sum()
-            seq_emb = embs[seq_num][: seq_len - 1]
-            # remove first <cls> token
-            embeddings.append(seq_emb[1:].cpu().numpy())
 
-        return embeddings
+
 
 
 
