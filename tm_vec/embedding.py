fe9ccb4f05969292ed51707797f6832ddd9e1247
valentynbez <walentyn.bzsz@gmail.com>
2024-05-16 11:17:38 +0200

    refractor: use abstractions to remove redundancy

--- tm_vec/embedding.py
+++ tm_vec/embedding.py
@@ -1,29 +1,17 @@
-import logging
-from abc import abstractmethod
-
-import torch
-from transformers import EsmModel, EsmTokenizer, T5EncoderModel, T5Tokenizer
-
-logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-
 class ProtLM:
-    """
-    Base class for protein LLMs based on BERT architecture.
-    Includes methods for tokenization and batching of sequences.
-    Tested on ESM and ProtT5
-    """
     def __init__(self, model_path, cache_dir, compile_model):
         self.model_path = model_path
         self.cache_dir = cache_dir
         self.compile_model = compile_model
         self.tokenizer = None
         self.model = None
-        self.device = torch.device(
-            "cuda" if torch.cuda.is_available() else "cpu")
+        self.device = None
+        self.forward_pass = None
 
     def finalize_torch(self):
+        import torch
+        self.device = torch.device(
+            "cuda" if torch.cuda.is_available() else "cpu")
         self.model.eval()
         self.model.to(self.device)
         if self.compile_model:
@@ -31,105 +19,79 @@ class ProtLM:
                                        mode="max-autotune",
                                        dynamic=True)
 
-    # # convert to half precision if cuda
-    # if self.device.type == "cuda":
-    #     self.model.half()
+        def forward_pass(self, inp):
+            """
+            Forward pass of the model.
+            """
+            with torch.no_grad():
+                out = self.model(**inp)
+            return out
+
+        self.forward_pass = forward_pass
 
     def tokenize(self, sequences):
         inp = self.tokenizer(sequences, padding=True, return_tensors="pt")
         return inp
 
-    def forward_pass(self, inp):
-        with torch.no_grad():
-            out = self.model(**inp)
-        return out
-
     def batch_embed(self, sequences):
-
         inp = self.tokenize(sequences)
         out = self.forward_pass(inp)
         embs = out.last_hidden_state.cpu().numpy()
-
         return inp, embs
 
-    @abstractmethod
-    def remove_special_tokens(self, embedding, attention_mask):
-        # add a verbose error
-        raise NotImplementedError(
-            "Method for removing special tokens is not implemented, "
-            "the embeddings should not be used as is.")
+    def remove_special_tokens(self, embeddings, attention_mask):
+        clean_embeddings = []
+        embeddings = list(embeddings)
+        for emb, mask in zip(embeddings, attention_mask):
+            seq_len = (mask == 1).sum()
+            seq_emb = emb[:seq_len - 1]
+            clean_embeddings.append(seq_emb)
+
+        return clean_embeddings
 
     def get_sequence_embeddings(self, sequences):
-        """
-        Get embeddings from the last layer for sequences.
-        """
         inp, embs = self.batch_embed(sequences)
-        # remove special tokens
         embs = self.remove_special_tokens(embs, inp["attention_mask"])
         return embs
 
 
-class ESMEncoder(ProtLM):
-    def __init__(self,
-                 model_path: str,
-                 cache_dir: str,
-                 compile_model: bool = False,
-                 local_files_only: bool = True,
-                 pooling_layer: bool = False):
-
-        super().__init__(model_path, cache_dir, compile_model)
-        self.model = EsmModel.from_pretrained(self.model_path,
-                                              cache_dir=self.cache_dir)
-        self.tokenizer = EsmTokenizer.from_pretrained(
-            self.model_path,
-            cache_dir=self.cache_dir,
-            local_files_only=local_files_only,
-            add_pooling_layer=pooling_layer)
-        super().finalize_torch()
-
-    def remove_special_tokens(self, embeddings, attention_mask):
-        """
-        Remove special tokens from the embedding
-        """
-        embeddings = list(embeddings)
-
-        clean_embeddings = []
-
-        for seq_num in range(len(embeddings)):
-            seq_len = (attention_mask[seq_num] == 1).sum()
-            seq_emb = embeddings[seq_num][:seq_len - 1]
-            # remove first <cls> token
-            clean_embeddings.append(seq_emb[1:])
-
-        return clean_embeddings
-
-
 class ProtT5Encoder(ProtLM):
     def __init__(self,
-                 model_path: str,
-                 cache_dir: str,
-                 compile_model: bool = False,
-                 local_files_only: bool = True,
-                 pooling_layer: bool = False):
+                 model_path,
+                 cache_dir,
+                 compile_model=False,
+                 local_files_only=True,
+                 pooling_layer=False):
+        from transformers import T5EncoderModel, T5Tokenizer
+        super().__init__(model_path, cache_dir, compile_model)
+        self.model = T5EncoderModel.from_pretrained(model_path,
+                                                    cache_dir=cache_dir)
+        self.tokenizer = T5Tokenizer.from_pretrained(
+            model_path, cache_dir=cache_dir, local_files_only=local_files_only)
+        self.finalize_torch()
+
+
+class ESMEncoder(ProtLM):
+    def __init__(self,
+                 model_path,
+                 cache_dir,
+                 compile_model=False,
+                 local_files_only=True,
+                 pooling_layer=False):
+        from transformers import EsmModel, EsmTokenizer
 
         super().__init__(model_path, cache_dir, compile_model)
-        self.model = T5EncoderModel.from_pretrained(self.model_path,
-                                                    cache_dir=self.cache_dir)
-        self.tokenizer = T5Tokenizer.from_pretrained(
-            self.model_path,
-            cache_dir=self.cache_dir,
-            local_files_only=local_files_only)
-        super().finalize_torch()
+        self.model = EsmModel.from_pretrained(model_path, cache_dir=cache_dir)
+        self.tokenizer = EsmTokenizer.from_pretrained(
+            model_path,
+            cache_dir=cache_dir,
+            local_files_only=local_files_only,
+            add_pooling_layer=pooling_layer)
+        self.finalize_torch()
 
     def remove_special_tokens(self, embeddings, attention_mask):
-        """
-        Remove special tokens from the embedding
-        """
-        clean_embeddings = []
-
-        for seq_num in range(len(embeddings)):
-            seq_len = (attention_mask[seq_num] == 1).sum()
-            seq_emb = embeddings[seq_num][:seq_len - 1]
-            clean_embeddings.append(seq_emb)
-
+        clean_embeddings = super().remove_special_tokens(
+            embeddings, attention_mask)
+        # remove <cls> token
+        clean_embeddings = [emb[1:] for emb in clean_embeddings]
         return clean_embeddings
